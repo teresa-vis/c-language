b8e11eee093d4f461c7229245f22e270b74b131e
teresa-vis <695319614@qq.com>
2024-08-24 11:32:18 +0800

    Add files via upload

--- pic.c
+++ pic.c
@@ -294,11 +294,535 @@
 //	printf("\n");
 //	return 0;
 //}
+//int main()
+//{//1.以这种方式表达不出矩阵的形式，足以证明它仅仅是逻辑上的概念。
+// //2.可以知道在内存中，各元素是连续存放的，不是二维的是线性的。
+//	int i=0,j=0;
+//	int a[3][4] = { {1},{2},{3} };
+//	printf("%d", a[i][j]);
+//	return 0;
+//}
+//将一个二维数组行和列的元素进行互换，存到另一个二维数组中。
+//int main()
+//{	
+//	int a[2][3] = { {1,2,3},{4,5,6} };
+//	int b[3][2], i, j;
+//	printf("array a:\n");
+//	for (i = 0; i <= 1; i++)
+//	{
+//		for (j = 0; j <= 2; j++)
+//		{
+//			printf("%d ", a[i][j]);
+//			b[j][i] = a[i][j];//在打印输出的过程中把它们之间的值进行转换。
+//		}
+//		printf("\n");
+//	}
+//	printf("array b:\n");//下面也就不需要进行i,j,下标之间的形式转化，因为下面在输出的时候，逻辑代码已经表示清楚了
+//	for (i = 0; i <= 2; i++)
+//	{
+//		for (j = 0; j <= 1; j++)
+//		{
+//			printf("%d ", b[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//有一个3 X 4的矩阵，求出其中最大的那个元素的值，以及其所在的行列号。
+//int main()
+//{	
+//	int i,j, max, row = 0, column = 0;
+//	int a[3][4] = { {1,2,3,4},{9,8,7,6},{-10,10,-5,2} };
+//	max = a[0][0];
+//	for (i = 0; i <= 2; i++)
+//	{
+//		for (j = 0; j <= 3; j++)
+//		{
+//			if (a[i][j] > max)
+//			{
+//				max = a[i][j];
+//				row = i;
+//				column = j;
+//			}
+//		}
+//	}
+//	printf("max=%d\nrow=%d\ncolumn=%d\n", max, row, column);
+//	return 0;
+//}
+//int main()
+//{
+//	int i, j, row = 0, column = 0, max;
+//	int a[4][5] = { {1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20} };
+//	max = a[0][0];
+//	for (i = 0; i <= 3; i++)
+//	{
+//		for (j = 0; j <= 4; j++)
+//		{
+//			if (max < a[i][j])
+//			{
+//				max = a[i][j];
+//				row = i;
+//				column = j;
+//			}
+//		}
+//	}
+//	printf("max=%d\nrow=%d\ncolumn=%d\n", max, row, column);
+//	return 0;
+//}
+//int main()
+//{	
+//	char a[][5] = { {' ',' ','*'},{' ','*',' ','*'},{'*',' ',' ',' ','*'},{' ','*',' ','*'},{' ',' ','*'} };
+//	int i, j;
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%c ", a[i][j]);
+//			
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{	
+//	char a[8] = "china";
+//	/*puts(a);
+//	gets();
+//	strlen("china");*/
+//	printf("%d",strlen("china"));
+//	return 0;
+//}
+//函数的调用
+//int main()
+//{	
+//	void print_star();
+//	void print_msg();
+//	print_star();
+//	print_msg();
+//	print_star();
+//	return 0;
+//}
+//void print_star()
+//{
+//	printf("*******************\n");
+//}
+//void print_msg()
+//{
+//	printf("this is a book\n");
+//}
+//输入两个数，要求输出其中值较大者。要求用函数来找到大数。
+//int main()
+//{	
+//	int max(int x, int y);
+//	int a, b, c;
+//	printf("输入两个数:");
+//	scanf("%d%d", &a, &b);
+//	c = max(a, b);
+//	printf("最大值是=%d ", c);
+//	return 0;
+//}
+//int max(int x, int y)
+//{
+//	int z;
+//	z = x > y ? x : y;
+//	return (z);
+//}
+//函数返回值的类型与指定的函数类型不同。
+//int max(float x, float y)
+//{
+//	float z;
+//	return(z = x > y ? x : y);
+//}
+//int main()
+//{	
+//	int max(float x,float y);
+//	float a, b;
+//	int c;
+//	scanf("%f%f", &a, &b);
+//	c = max(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+//输入两个实数，用一个函数求出它们之和。
+//int main()
+//{	
+//	float add(float , float );
+//	float a, b, c;
+//	printf("enter a and b:\n");
+//	scanf("%f,%f", &a, &b);
+//	c = add(a, b);
+//	printf("sum is %f\n", c);
+//	return 0;
+//}
+//float add(float x, float y)
+//{
+//	float z;
+//	z = x + y;
+//	return(z);
+//}
+//求出4个数的最大值，要求用函数嵌套的方式
+//int main()
+//{
+//	int max4(int a, int b, int c, int d);
+//	int a, b, c, d, max;
+//	printf("please enter four integer numbers:\n");
+//	scanf("%d%d%d%d", &a, &b, &c, &d);
+//	max = max4(a, b, c, d);
+//	printf("max=%d", max);
+//	return 0;
+//}
+//int max4(int a, int b, int c, int d)
+//{
+//	int max2(int a, int b);
+//	int m;
+//	m = max2(a, b);
+//	m = max2(m, c);
+//	m = max2(m, d);
+//	return m;
+//}
+//int max2(int a, int b)
+//{
+//	if (a >= b)
+//	{
+//		return a;
+//	}
+//	else {
+//		return b;
+//	}
+//}
+//程序改进
+//int main()
+//{
+//	int max4(int a, int b, int c, int d);
+//	int a, b, c, d, max;
+//	printf("please enter four integer numbers:\n");
+//	scanf("%d%d%d%d", &a, &b, &c, &d);
+//	max = max4(a, b, c, d);
+//	printf("max=%d", max);
+//	return 0;
+//}
+//int max4(int a, int b, int c, int d)
+//{
+//	int max2(int a, int b);
+//	return max2(max2(max2(a, b), c), d);//把函数调用作为函数的参数
+//}
+//int max2(int a, int b)
+//{
+//	return(a >= b ? a : b);//运用条件表达式进行简化
+//}
+//理解函数的递归调用
+//int main()
+//{	
+//	int age(int n);
+//	/*int m;
+//	m = age(5);
+//	printf("NO.5,age:%d\n", m);*/
+//	printf("NO.5,age:%d\n", age(5));
+//	return 0;
+//}
+//int age(int n)
+//{
+//	int c;
+//	if (n == 1)
+//	{
+//		c = 10;
+//	}
+//	else {
+//		c = age(n - 1) + 2;
+//	}
+//	return c;
+//}
+//用递归的方法求n!
+//int main()
+//{	
+//	int fac(int n);
+//	int n, m;
+//	printf("input an integer number:\n");
+//	scanf("%d", &n);
+//	m = fac(n);
+//	printf("%d!=%d\n", n,m);
+//	return 0;
+//}
+//int fac(int n)
+//{
+//	int f;
+//	if (n < 0)
+//	{
+//		printf("n<0,data error!");
+//	}
+//	else if (n == 0 || n == 1)
+//	{
+//		f = 1;
+//	}
+//	else
+//	{
+//		f = fac(n - 1) * n;
+//	}
+//	return (f);
+//}
+//
+//练习一
+//int main()
+//{
+//	int toup(int n);
+//	int n, m;
+//	printf("input an integer number:\n");
+//	scanf("%d", &n);
+//	m = toup(n);
+//	printf("%d!=%d\n", n, m);
+//	return 0;
+//}
+//int toup(int n)
+//{
+//	int c;
+//	if (n < 0)
+//	{
+//		printf("n<0,data error:\n");
+//	}
+//	else if (n == 0 || n == 1)
+//	{
+//		c = 1;
+//	}
+//	else {
+//		c = toup(n - 1) * n;
+//	}
+//	return c;
+//}
+//Hanoi(汉诺)塔问题。
+//int main()
+//{	
+//	void mot(int n, char one, char two, char three);
+//	int m;
+//	printf("输入盘数:");
+//	scanf("%d", &m);
+//	printf("移动的步数%d为:\n", m);
+//	mot(m, 'A', 'B', 'C');
+//	return 0;	
+//}
+//void mot(int n, char one, char two, char three)
+//{
+//	void move(char x, char y);
+//	if (n == 1)
+//	{
+//		move(one, three);
+//	}
+//	else
+//	{
+//		mot(n - 1, one, three, two);
+//		move(one, three);
+//		mot(n - 1, two, one, three);
+//	}
+//}
+//void move(char x, char y)
+//{
+//	printf("%c-->%c\n", x, y);
+//}
+// 数组元素做函数的参数。
+//输入十个数，并求出其中最大的元素和该数是第几个数。
+//int main()
+//{	
+//	int compare(int x, int y);
+//	int a[10],m,n,i;
+//	printf("输入十个数:");
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	printf("\n");
+//	for (i = 1, m = a[0], n = 0; i < 10; i++)
+//	{
+//		if (compare(m, a[i]) > m)
+//		{
+//			m = compare(m, a[i]);
+//			n = i;
+//		}
+//	}
+//	printf("最大的数是:%d\n第%d个是最大的数:\n", m, n + 1);
+//	return 0;
+//}
+//int compare(int x, int y)
+//{
+//	return(x > y ? x : y);
+//}
+//int main()
+//{
+//	int max(int x, int y);
+//	int a[10], m, n, i;
+//	printf("输入十个数:\n");
+//	for(i=0;i<10;i++)
+//	scanf("%d", &a[i]);
+//	printf("\n");
+//	for (i = 1, n = 0,m = a[0]; i < 10; i++)
+//	{
+//		if (max(a[i], m)>m)
+//		{
+//			m = max(a[i], m);
+//			n = i;
+//		}
+//	}
+//	printf("输出的最大值是:%d\n第%d个数是最大值\n", m,n+1);
+//	return 0;
+//}
+//int max(int x, int y)
+//{
+//	return(x > y ? x : y);
+//}
+// 一维数组名作函数的参数
+// 求十名学生的成绩并返回。
+//int main()
+//{	
+//	float average(float arr[10]);
+//	float score[10], aver;
+//	int i;
+//	printf("输入十名学生的成绩:\n");
+//	for (i = 0; i < 10; i++)
+//		scanf("%f", &score[i]);
+//	aver = average(score);
+//	printf("%5.2f", aver);
+//	return 0;
+//}
+//float average(float arr[10])
+//{
+//	int i;
+//	float aver, sum = arr[0];
+//	for (i = 1; i < 10; i++)
+//	{
+//		sum = sum + arr[i];
+//		aver = sum / 10;
+//	}
+//	return aver;
+//}
+//练习
+//int main()
+//{	
+//	float averge(float arr[10]);
+//	float score[10], aver;
+//	int i;
+//	printf("输入十名学生的成绩:\n");
+//	for (i = 0; i < 10; i++)
+//		scanf("%f", &score[i]);
+//	printf("\n");
+//	aver = averge(score);
+//	printf("平均成绩为%5.2f", aver);
+//	return 0;
+//}
+//float averge(float arr[10])
+//{
+//	int i;
+//	float aver, sum = arr[0];
+//	for (i = 1; i < 10; i++)
+//	{
+//		sum = sum + arr[i];
+//		aver = sum / 10;
+//	}
+//	return aver;
+//}
+//设两个班级学生人数为5人和10人，调用一个average函数，分别求出两个班级的平均成绩
+//int main()
+//{	
+//	float average(float arr[], int n);
+//	float score1[5] = { 98.5,97,91.5,60,55 };
+//	float score2[10] = { 67.5,89.5,99,69.5,77,89.5,76.5,54,60,99.5 };
+//	printf("平均成绩A是%6.2f\n", average(score1, 5));
+//	printf("平均成绩B是%6.2f\n", average(score2, 10));
+//	return 0;
+//}
+//float average(float arr[], int n)
+//{
+//	int i;
+//	float aver, sum = arr[0];
+//	for (i = 1; i < n; i++)
+//	{
+//		sum = sum + arr[i];
+//		aver = sum / n;
+//	}
+//	return (aver);
+//}
+//************************************用选择法对数组中的10个整数按由小到大排序。
+//int main()
+//{	
+//	void sort(int array[], int n);
+//	int i, a[10];
+//	printf("输入十个整数:\n");
+//	for (i = 0; i < 10; i++)
+//		scanf("%d", &a[i]);
+//	sort(a, 10);
+//	printf("排序后的数组为:\n");
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//	return 0;
+//}
+//void sort(int array[], int n)
+//{
+//	int i, j, k, t;
+//	for (i = 0; i < n - 1; i++)
+//	{
+//		k = i;
+//		for (j = i + 1; j < n; j++)
+//		{
+//			if (array[j] < array[k])
+//			{
+//				k = j;
+//				
+//			}
+//		}
+//		t = array[k];
+//		array[k] = array[i];
+//		array[i] = t;
+//	}
+//}
+//多维数组名做函数的参数
+//int main()
+//{
+//	int max_value(int arr[][4]);
+//	int arr[3][4] = { {12,15,17,3},{4,9,8,3},{7,21,42,62} };
+//	printf("最大值是:%d\n",max_value(arr));
+//	return 0;
+//}
+//int max_value(int arr[][4])
+//{
+//	int i, j, max;
+//	max = arr[0][0];
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			if (max < arr[i][j])
+//			{
+//				max = arr[i][j];
+//			}
+//		}
+//	}
+//	return(max);
+//}
+//写一个函数，当主函数调用此函数后，能求出平均分，最高分，最低分。(程序有误)
+float Max = 0, Min = 0;
 int main()
-{//1.以这种方式表达不出矩阵的形式，足以证明它仅仅是逻辑上的概念。
- //2.可以知道在内存中，各元素是连续存放的，不是二维的是线性的。
-	int i=0,j=0;
-	int a[3][4] = { {1},{2},{3} };
-	printf("%d", a[i][j]);
+{	
+	float average(float array[], int n);
+	float ave, score[10];
+	int i;
+	printf("enter 10 scores:");
+	for (i = 0; i < 10; i++)
+		scanf("%f", &score[i]);
+	ave = average(score, 10);
+	printf("max=%6.2f\nmin=%6.2f\naverage=%6.2f\n", Max, Min, ave);
 	return 0;
+}
+float average(float array[], int n)
+{
+	int i;
+	float aver, sum = array[0];
+	Max = Min = array[0];
+	for (i = 1; i < n; i++)
+	{
+		if (array[i] > Max)Max = array[i];
+		else if (array[i] < Min)Min = array[i];
+		sum = sum + array[i];
+	}
+	aver = sum / n;
+	return aver;
 }
