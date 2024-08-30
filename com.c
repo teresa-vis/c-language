6a405bc75c955a26a39a0687642ba1ce88c7273e
teresa-vis <695319614@qq.com>
2024-08-30 16:51:38 +0800

    Add files via upload

--- com.c
+++ com.c
@@ -107,3 +107,82 @@
 //C语言中实参变量和形参变量之间的数据传递是单项的"值传递"方式，用指针变量做函数的参数是同样要遵循这一原则，不可能通过执行调用函数来改变
 //实参指针变量的值，但是可以改变实参指针变量所指变量的值。
 
+//输入三个整数a,b,c要求按由大到小的顺序输出，用swap函数交换两个变量的值
+//用exchange函数改变这三个数的值。
+//int main()
+//{
+//	void exchange(int* x, int* y, int* z);
+//	int a, b, c;
+//	printf("please enter three integer number:\n");
+//	scanf("%d,%d,%d", &a, &b, &c);
+//	int* A = &a, * B = &b, * C = &c;
+//	exchange(A, B, C);
+//	printf("exchange%d,%d,%d\n", a, b, c);
+//	return 0;
+//}
+//void exchange(int* x, int* y, int* z)
+//{
+//	void swap(int* m, int* n);
+//	if (*x < *y)swap(x, y);//注意：这里函数参数是值的传递，不是地址（指针）的传递。
+//	if (*x < *z)swap(x, z);
+//	if (*y < *z)swap(y, z);
+//}
+//void swap(int* m, int* n)
+//{
+//	int temp;
+//	temp = *m;
+//	*m = *n;
+//	*n = temp;
+//}
+// 练习two
+//int main()
+//{
+//	void exchange(int* q1, int* q2, int* q3);
+//	int a, b, c,* p1, * p2, * p3;
+//	printf("please enter three number:");
+//	scanf("%d,%d,%d", &a, &b, &c);
+//	p1 = &a; p2 = &b; p3 = &c;
+//	exchange(p1, p2, p3);
+//	printf("the order is:%d,%d,%d\n", a, b, c);
+//	return 0;
+//}
+//void exchange(int* q1, int* q2, int* q3)
+//{
+//	void swap(int* pt1, int* pt2);
+//	if (*q1 < *q2)swap(q1, q2);
+//	if (*q1 < *q3)swap(q1, q3);
+//	if (*q2 < *q3)swap(q2, q3);
+//}
+//void swap(int* pt1, int* pt2)
+//{
+//	int temp;
+//	temp = *pt1;
+//	*pt1 = *pt2;
+//	*pt2 = temp;
+//}
+//练习three
+//int main()
+//{
+//	void exchange(int* q1, int* q2, int* q3);
+//	int a, b, c;
+//	printf("please enter three integer number:");
+//	scanf("%d,%d,%d", &a, &b, &c);
+//	int* p1 = &a, * p2 = &b, * p3 = &c;
+//	exchange(p1, p2, p3);
+//	printf("the order is:%d,%d,%d", a, b, c);
+//	return 0;
+//}
+//void exchange(int* q1, int* q2, int* q3)
+//{
+//	void swap(int* pt1, int* pt2);
+//	if (*q1 < *q2)swap(q1 , q2);
+//	if (*q1 < *q3)swap(q1, q3);
+//	if (*q2 < *q3)swap(q2, q3);
+//}
+//void swap(int* pt1, int* pt2)
+//{
+//	int temp;
+//	temp = *pt1;
+//	*pt1 = *pt2;
+//	*pt2 = temp;
+//}
