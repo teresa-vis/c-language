0943b7d8d8f1fdbb911308261a027ff60eb52aad
teresa-vis <695319614@qq.com>
2024-09-23 19:00:53 +0800

    Add files via upload

--- nine.c
+++ nine.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 //int main()
 //{
 //	void sort(int arry[], int n);
@@ -468,3 +469,88 @@
 //	} while (p != NULL);
 //	return 0;
 //}
+//建立动态链表
+//#define len sizeof(struct Student)
+//struct Student
+//{
+//	long num;
+//	float score;
+//	struct Student* next;
+//};
+//int n;
+//struct Student* creat(void)
+//{
+//	struct Student* head;
+//	struct Student* p1, * p2;
+//	n = 0;
+//	p1 = p2 = (struct Student*)malloc(len);
+//	scanf("%d%f", &p1->num, &p1->score);
+//	head = NULL;
+//	while (p1->num != 0)
+//	{
+//		n = n + 1;
+//		if (n == 1)head = p1;
+//		else p2->next = p1;
+//		p2 = p1;
+//		p1 = (struct Student*)malloc(len);
+//		scanf("%d%f", &p1->num, &p1->score);
+//	}
+//	p2->next = NULL;
+//	return head;
+//}
+//int main()
+//{
+//	struct Student* pt;
+//	pt = creat();
+//	printf("%d %0.2f", pt->num, pt->score);
+//	return 0;
+//}
+//输出链表
+#include <malloc.h>
+#define len sizeof(struct Student)
+struct Student
+{
+	long num;
+	float score;
+	struct Student* next;
+};
+int n;
+struct Student* creat()
+{
+	struct Student* head;
+	struct Student* p1, * p2;
+	n = 0;
+	p1 = p2 = (struct Student*)malloc(len);
+	scanf("%ld%f", &p1->num, &p1->score);
+	head = NULL;
+	while (p1->num != 0)
+	{
+		n = n + 1;
+		if (n == 1)head = p1;
+		else p2->next = p1;
+		p2 = p1;
+		p1 = (struct Student*)malloc(len);
+		scanf("%ld%f", &p1->num, &p1->score);
+	}
+	p2->next = NULL;
+	return head;
+}
+void print(struct Student* head)
+{
+	struct Student* p;
+	printf("\nNow those %d records are:\n", n);
+	p = head;
+	if(head!=NULL)
+		do
+		{
+			printf("%ld %0.2f\n", p->num, p->score);
+			p = p->next;
+		} while (p != NULL);
+}
+int main()
+{
+	struct Student* head;
+	head = creat();
+	print(head);
+	return 0;
+}
